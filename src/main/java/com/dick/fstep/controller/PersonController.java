f8445064ce1c2d7afe5f2399e0ec0ac9708b3451
wenxuanli <CallmeDick7>
2018-12-14 17:14:38 +0800

    commit

--- src/main/java/com/dick/fstep/controller/PersonController.java
+++ src/main/java/com/dick/fstep/controller/PersonController.java
@@ -5,7 +5,9 @@ import com.dick.fstep.service.PersonService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -17,6 +19,7 @@ public class PersonController {
 
     @Autowired
     private PersonService personService;
+
     @RequestMapping("/selectPerson")
     public void selectPerson(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
@@ -30,5 +33,14 @@ public class PersonController {
 
         response.getWriter().write(mapper.writeValueAsString(person));
         response.getWriter().close();
+    }
+
+    @RequestMapping("/testController")
+    @ResponseBody
+    public Object testController(@RequestBody Person dto,HttpServletResponse response) throws IOException {
+        System.out.println("---------------------");
+        System.out.println(dto.getId());
+        return dto;
+        //response.getWriter().write(dto.getId()+"");
     }
 }
