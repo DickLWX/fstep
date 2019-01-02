055b24201bfb8f4f101dad2c5cbb2bc64847e134
wenxuanli <CallmeDick7>
2019-01-02 17:19:21 +0800

    commit

---
+++ src/main/java/com/dick/fstep/test/lambda/Person.java
@@ -0,0 +1,21 @@
+package com.dick.fstep.test.lambda;
+
+import lombok.Data;
+
+@Data
+public class Person {
+
+    private String firstName, lastName, job, gender;
+    private int salary, age;
+
+    public Person(String firstName, String lastName, String job,
+                  String gender, int age, int salary)       {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.gender = gender;
+        this.age = age;
+        this.job = job;
+        this.salary = salary;
+    }
+
+}
---
+++ src/main/java/com/dick/fstep/test/thread/ThreadPoolTest.java
@@ -0,0 +1,49 @@
+package com.dick.fstep.test.thread;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+public class ThreadPoolTest {
+
+    public static void main(String[] args) {
+        ThreadPoolExecutor executor = new ThreadPoolExecutor(5, 10, 200, TimeUnit.MILLISECONDS,
+                new ArrayBlockingQueue<Runnable>(5));
+        /*Executors.newCachedThreadPool();        //创建一个缓冲池，缓冲池容量大小为Integer.MAX_VALUE
+        Executors.newSingleThreadExecutor();   //创建容量为1的缓冲池
+        Executors.newFixedThreadPool(1000);*/  // 一般不直接调构造方法， 使用如上3个静态方法
+        /*newFixedThreadPool创建的线程池corePoolSize和maximumPoolSize值是相等的，它使用的LinkedBlockingQueue；
+
+　　newSingleThreadExecutor将corePoolSize和maximumPoolSize都设置为1，也使用的LinkedBlockingQueue；
+
+　　newCachedThreadPool将corePoolSize设置为0，将maximumPoolSize设置为Integer.MAX_VALUE，使用的SynchronousQueue，也就是说来了任务就创建线程运行，当线程空闲超过60秒，就销毁线程。*/
+        for(int i=0;i<15;i++){
+            MyTask myTask = new MyTask(i);
+            executor.execute(myTask);
+            System.out.println("线程池中线程数目："+executor.getPoolSize()+"，队列中等待执行的任务数目："+
+                    executor.getQueue().size()+"，已执行玩别的任务数目："+executor.getCompletedTaskCount());
+        }
+        executor.shutdown();
+    }
+}
+
+
+class MyTask implements Runnable {
+    private int taskNum;
+
+    public MyTask(int num) {
+        this.taskNum = num;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("正在执行task "+taskNum);
+        try {
+            Thread.currentThread().sleep(4000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("task "+taskNum+"执行完毕");
+    }
+}
